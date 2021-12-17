c11c5bde05155d498bec95dbeca1d0133c6e03b4
hummingbirdsday <info@xrealityacademy.com>
2021-12-16 17:20:51 -0800

    Distance Grab_3_ Inventory
    Inventory

---
+++ VR Setup_02/Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+
+public class InventoryItem : MonoBehaviour
+{
+    public InventoryItemData data { get; private set; }
+    public int stackSize { get; private set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR Setup_02/Assets/Scripts/InventorySystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    private Dictionary<InventoryItemData, InventoryItem> m_itemDictionary;
+    public List<InventoryItem> inventory { get; private set; }
+
+    private void Awake()
+    {
+       inventory = new List<InventoryItem>();
+       m_itemDictionary = new Dictionary<InventoryItemData, InventoryItem>();
+        
+
+    }
+
+    public void Add(InventoryItemData referenceData)
+    {
+      if (m_itemDictionary.TryGetValue(referenceData, out InventoryItem value))
+         {
+            value.AddtoStack();
+         }
+      else
+      {
+         InventoryItem newItem = new InventoryItem(referenceData);
+         inventory.Add(newItem);
+         m_itemDictionary.Add(referenceData, newItem);
+      }
+        
+            
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
