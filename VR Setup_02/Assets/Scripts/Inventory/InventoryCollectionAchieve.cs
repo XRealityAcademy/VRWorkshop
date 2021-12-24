1f5132fef096e0a274e1af9689cf833dd3484818
hummingbirdsday <info@xrealityacademy.com>
2021-12-23 20:31:26 -0800

    Snap+Inventory

---
+++ VR Setup_02/Assets/Scripts/Inventory/InventoryCollectionAchieve.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryCollectionAchieve : MonoBehaviour
+{
+    //Create array of game objects
+    public GameObject[] InventoryItems;
+
+    public GameObject goalAcheivePanel;
+    //Used to check if all parts have been added to the rocket
+    private bool partAdded;
+    //Flag to ensure the rocket only launches once
+    private bool launched = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if all three parts have been snapped into place, and make sure we haven't already launched
+        if (fullyCollected() == true && launched == false)
+        {
+            goalAcheivePanel.SetActive(true);
+        }
+    }
+    
+    //Use forloop to iterate through the array of inventory items
+    //Checks to see if isSnapped is set to true on each one
+    //Return false if any one of the three is false
+    //If all three return true, then set to true
+    // ReSharper disable Unity.PerformanceAnalysis
+    private bool fullyCollected()
+    {
+        for (int i= 0; i < InventoryItems.Length; i++)
+        {
+            partAdded = InventoryItems[i].GetComponent<SnapObject>().isSnapped;
+            if (partAdded == false)
+            {
+                return false;
+            }
+            
+        }
+
+        return true;
+    }
+}
